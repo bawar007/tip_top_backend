b619f27b616a4e3d73a8730d20dcbecba49a43fe
bawar007 <barski001@o2.pl>
2023-04-04 19:08:11 +0200

    m

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import hsp from "heroku-self-ping";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
-hsp("https://bawar007.github.io/tip_top/");
+require("heroku-self-ping").default("https://bawar007.github.io/tip_top");
 
 app.listen(5000, () => console.log("Server up and running..."));
