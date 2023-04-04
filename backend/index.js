8006c22017d019a1ee55ca9682fa22d29be69c89
bawar007 <barski001@o2.pl>
2023-04-04 19:43:22 +0200

    fix

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
-require("heroku-self-ping").default("https://bawar007.github.io/tip_top");
+// require("heroku-self-ping").default("https://bawar007.github.io/tip_top");
 
 app.listen(5000, () => console.log("Server up and running..."));
