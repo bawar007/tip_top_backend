c0392c339b06db294f8c32395e713e410bfa6cc1
bawar007 <barski001@o2.pl>
2023-04-23 21:22:26 +0200

    delete interval

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,10 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import { getOpinions } from "./controllers/UserController.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
-
-// setInterval(() => {
-//   const r = express.Router();
-//   r.get("/opinions", getOpinions);
-//   console.log("Ping");
-// }, 10000);
 
 app.listen(5000, () => console.log("Server up and running..."));
